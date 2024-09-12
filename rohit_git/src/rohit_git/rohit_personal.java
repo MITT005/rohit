06623d7d4e181bc3e37e0d2b8538c72689232551
MITT005 <rohit.mittal.ext@sonymusic.com>
2024-09-12 11:06:01 +0530

    first comment.

---
+++ rohit_git/src/rohit_git/rohit_personal.java
@@ -0,0 +1,15 @@
+package rohit_git;
+
+public class rohit_personal {
+
+	public static void main(String[] args) {
+		
+		
+	  String Orignal_string = "HELLO";
+	  String reversed_string = "";
+	  System.out.println(Orignal_string);
+		// TODO Auto-generated method stub
+
+	}
+
+}
