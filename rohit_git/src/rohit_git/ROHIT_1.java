17bff3cebe4bce196511c3ad9ea27df6186edb92
MITT005 <rohit.mittal.ext@sonymusic.com>
2024-09-12 11:37:14 +0530

    New Updated file

---
+++ rohit_git/src/rohit_git/ROHIT_1.java
@@ -0,0 +1,15 @@
+package rohit_git;
+
+public class ROHIT_1 {
+
+	public static void main(String[] args) {
+		
+		
+		  String Orignal_string = "HELLO";
+		  String reversed_string = "";
+		  System.out.println(Orignal_string);
+			// TODO Auto-generated method stub
+
+		}
+
+	}
